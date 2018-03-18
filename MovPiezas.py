bf2ddb6353ee1ee23906827b6e2fd461724f6ba7
H3Caballo <37317219+H3Caballo@users.noreply.github.com>
2018-03-18 00:04:19 -0600

    Reina y EnPassant
    Modifiqué la función del caballo, creé la de la reina y modifiqué el peón para movimiento enPassant

--- MovPiezas.py
+++ MovPiezas.py
@@ -1,14 +1,21 @@
-#Ricardo Torres Sánchez
-def moverPeonB(indice,coordDestino):
+#Ricardo Torres Sánchez A01334302
+def moverPeonB(indice,coordDestino,enpassantN,columnaEnpassantN,indiceEnpassantN):
     mover=0
+    enpassantB=False
+    columnaEnpassantB=0
+    indiceEnpassantB=0
     coordOrigen=posBlancas[indice]
     casillaOcupada=False
     if coordOrigen&0xF==2 and (coordDestino&0xF) - (coordOrigen&0xF) == 2 and (coordDestino&0xF0) - (coordOrigen&0xF0) == 0 : 
         accion='mover'
+        enpassantB=True
+        columnaEnpassantB=coordOrigen&0xF0
+        indiceEnpassantB=indice
     elif (coordDestino&0xF) - (coordOrigen&0xF) == 1 :
         if (coordDestino&0xF0) - (coordOrigen&0xF0) == 0 :
             accion='mover'
         elif (coordDestino&0xF0) - (coordOrigen&0xF0) == 0x10 or (coordDestino&0xF0) - (coordOrigen&0xF0) == -0x10:
+            #Una vez probando que funciona, probar con elif abs((coordDestino&0xF0) - (coordOrigen&0xF0)) == 0x10:
             accion='comer'
         else:
             accion='invalido'
@@ -28,6 +35,7 @@ def moverPeonB(indice,coordDestino):
                     casillaOcupada=True
                     break
             if casillaOcupada:
+                enpassantB=False
                 break
         if vueltas == 0 and not(casillaOcupada):
             mover=coordDestino
@@ -42,14 +50,93 @@ def moverPeonB(indice,coordDestino):
                 break
         if casillaOcupada:
             mover=coordDestino
+            #Accion motor pasos
             posBlancas[indice]=coordDestino
             posNegras[cont]=posNegrasMorts[cont]
+        elif enpassantN:
+            if columnaEnpassantN == coordDestino&0xF0:
+                mover=coordDestino
+                #Accion motor pasos
+                posBlancas[indice]=coordDestino
+                posNegras[indiceEnpassantN]=posNegrasMorts[indiceEnpassantN]
         else:
             accion='invalido'
-    print(accion)
-    ##print(coordOrigen)
-    ##print(cont)
-    return
+    #print(accion)
+    enpassantN=False
+    return accion,enpassantB,columnaEnpassantB,indiceEnpassantB
+
+def moverPeonN(indice,coordDestino,enpassantB,columnaEnpassantB,indiceEnpassantB):
+    mover=0
+    enpassantN=False
+    columnaEnpassantN=0
+    indiceEnpassantN=0
+    coordOrigen=posNegras[indice]
+    casillaOcupada=False
+    if coordOrigen&0xF==7 and (coordOrigen&0xF) - (coordDestino&0xF) == 2 and (coordDestino&0xF0) - (coordOrigen&0xF0) == 0 : 
+        accion='mover'
+        enpassantN=True
+        columnaEnpassantN=coordOrigen&0xF0
+        indiceEnpassantN=indice
+    elif (coordOrigen&0xF) - (coordDestino&0xF) == 1 :
+        if (coordDestino&0xF0) - (coordOrigen&0xF0) == 0 :
+            accion='mover'
+        elif (coordDestino&0xF0) - (coordOrigen&0xF0) == 0x10 or (coordDestino&0xF0) - (coordOrigen&0xF0) == -0x10:
+            #Una vez probando que funciona, probar con elif abs((coordDestino&0xF0) - (coordOrigen&0xF0)) == 0x10:
+            accion='comer'
+        else:
+            accion='invalido'
+    else:
+        accion='invalido'
+    
+    vueltas = (coordOrigen&0xF) - (coordDestino&0xF)
+    
+    if accion=='mover':
+        while vueltas > 0:
+            vueltas -= 1
+            for cont in range(0,16):
+                if posBlancas[cont] == coordDestino + vueltas:
+                    casillaOcupada=True
+                    break
+                if posNegras[cont] == coordDestino + vueltas:
+                    casillaOcupada=True
+                    break
+            if casillaOcupada:
+                enpassantN=False
+                break
+        if vueltas == 0 and not(casillaOcupada):
+            mover=coordDestino
+            posNegras[indice]=coordDestino
+            ##Accion motor pasos
+        else:
+            accion='invalido'
+    elif accion=='comer':
+        for cont in range(0,15):
+            if posBlancas[cont] == coordDestino:
+                casillaOcupada=True
+                break
+        if casillaOcupada:
+            mover=coordDestino
+            #Accion motor pasos
+            posNegras[indice]=coordDestino
+            posBlancas[cont]=posBlancasMorts[cont]
+        elif enpassantB:
+            if columnaEnpassantB == coordDestino&0xF0:
+                mover=coordDestino
+                #Accion motor pasos
+                posNegras[indice]=coordDestino
+                posBlancas[indiceEnpassantB]=posBlancasMorts[indiceEnpassantB]
+        else:
+            accion='invalido'
+    #print(accion)
+    enpassantB=False
+    return accion,enpassantN,columnaEnpassantN,indiceEnpassantN
+
+def moverPeon(indice,coordDestino,turnoBlancas,enpassantB,columnaEnpassantB,indiceEnpassantB,enpassantN,columnaEnpassantN,indiceEnpassantN):
+    if turnoBlancas:
+        accion=moverPeonB(indice,coordDestino,enpassantN,columnaEnpassantN,indiceEnpassantN)
+    else:
+        accion=moverPeonN(indice,coordDestino,enpassantB,columnaEnpassantB,indiceEnpassantB)
+    return accion
 
 def moverTorre(indice,coordDestino,turnoBlancas):
     mover=0
@@ -63,8 +150,8 @@ def moverTorre(indice,coordDestino,turnoBlancas):
     
     if coordDestino==coordOrigen:
         accion='invalido'
-        print(accion)
-        return
+        #print(accion)
+        return accion
     elif (coordDestino&0xF0) - (coordOrigen&0xF0) == 0:
         accion='moverVertical'
         vueltas = (coordDestino&0xF) - (coordOrigen&0xF)
@@ -73,13 +160,10 @@ def moverTorre(indice,coordDestino,turnoBlancas):
         vueltas = (coordDestino&0xF0) - (coordOrigen&0xF0)
     else:
         accion='invalido'
-        print(accion)
-        return
+        #print(accion)
+        return accion
     if vueltas<0:
         signo=-1
-    ##print(vueltas)
-    ##print(accion)
-    ##print(signo)
     
     if accion=='moverVertical':
         while vueltas != 0:
@@ -149,8 +233,8 @@ def moverTorre(indice,coordDestino,turnoBlancas):
                 posBlancas[cont]=posBlancasMorts[cont]
         else:
             accion='invalido'
-    print(accion)
-    return
+    #print(accion)
+    return accion
 
 def moverAlfil(indice,coordDestino,turnoBlancas):
     mover=0
@@ -212,73 +296,70 @@ def moverAlfil(indice,coordDestino,turnoBlancas):
         else:
             accion='invalido'
     else:
-        print(accion)
-        return
-    print(accion)
-    return
+        #print(accion)
+        return accion
+    #print(accion)
+    return accion
         
-def moverCaballo(coordOrigen, coordDestino, turnoBlancas):
+def moverCaballo(indice, coordDestino, turnoBlancas):
     mover=0
+    if turnoBlancas:
+        coordOrigen=posBlancas[indice]
+    else:
+        coordOrigen=posNegras[indice]
+
     casillaOcupada=False
-    if (coordDestino&0xF) - (coordOrigen&0xF) == 2 and ((coordDestino&0xF0) - (coordOrigen&0xF0) == 0x10 or (coordDestino&0xF0) - (coordOrigen&0xF0) == -0x10):
-        accion='moverAdelante'
-    elif (coordDestino&0xF) - (coordOrigen&0xF) == -2 and ((coordDestino&0xF0) - (coordOrigen&0xF0) == 0x10 or (coordDestino&0xF0) - (coordOrigen&0xF0) == -0x10):
-        accion='moverAtras'
-    elif ((coordDestino&0xF) - (coordOrigen&0xF) == 1 or (coordDestino&0xF) - (coordOrigen&0xF) == -1) and (coordDestino&0xF0) - (coordOrigen&0xF0) == 0x20:
-        accion='moverDerecha'
-    elif ((coordDestino&0xF) - (coordOrigen&0xF) == 1 or (coordDestino&0xF) - (coordOrigen&0xF) == -1) and (coordDestino&0xF0) - (coordOrigen&0xF0) == -0x20:
-        accion='moverIzquierda'
+    
+    difFilas=(coordDestino&0xF) - (coordOrigen&0xF)
+    difColumnas=(coordDestino&0xF0) - (coordOrigen&0xF0)
+    
+    if abs(difFilas) == 2 and abs(difColumnas) == 0x10 or abs(difFilas) == 1 and abs(difColumnas) == 0x20:
+        accion='mover'
     else:
         accion='invalido'
-
-    comer=0              ## casilla desocupada = 0 y casilla ocupada por pieza opuesta = 1
-    if accion=='moverAdelante' or accion=='moverAtras' or accion=='moverDerecha' or accion=='moverIzquierda':
-        if turnoBlancas==0:
-            for cont in range(0,16):
-                if posBlancas[cont] == coordDestino:
-                    comer=0
-                    casillaOcupada=True
-                    break
-                if posNegras[cont]-0x80 == coordDestino:
-                    comer=1
-                    casillaOcupada=True
-                    break
-            if comer==0:
-                if casillaOcupada==True:
+        
+    if accion=='mover':
+        for cont in range(0,16):
+            if posBlancas[cont] == coordDestino:
+                if turnoBlancas:
                     accion='invalido'
-                    mover=coordDestino
                 else:
-                    accion='mover'
-                    mover=coordDestino
-            else:
-                accion='comer'
-                mover=coordDestino
+                    accion='comer'
+                break
+            if posNegras[cont] == coordDestino:
+                if not(turnoBlancas):
+                    accion='invalido'
+                else:
+                    accion='comer'
+                break
+    if accion=='comer':
+        if turnoBlancas:
+            posBlancas[indice]=coordDestino
+            posNegras[cont]=posNegrasMorts[cont]
         else:
-            for cont in range(0,16):
-                if posBlancas[cont] == coordDestino:
-                    comer=1
-                    casillaOcupada=True
-                    break
-                if posNegras[cont]-0x80 == coordDestino:
-                    comer=0
-                    casillaOcupada=True
-                    break
-            if comer==1:
-                accion='mover'
-                mover=coordDestino
-            else:
-                if casillaOcupada==True:
-                    accion='invalido'
-                    mover=coordDestino
-                else:
-                    accion='mover'
-                    mover=coordDestino
+            posNegras[indice]=coordDestino
+            posBlancas[cont]=posBlancasMorts[cont]
+
+    #print(accion)
+    return accion
+
+def moverReina(indice, coordDestino, turnoBlancas):
+    if turnoBlancas:
+        coordOrigen=posBlancas[indice]
+    else:
+        coordOrigen=posNegras[indice]
+    
+    difFilas=(coordDestino&0xF) - (coordOrigen&0xF)
+    difColumnas=(coordDestino&0xF0) - (coordOrigen&0xF0)
+    
+    if difFilas==0 or difColumnas==0:
+        accion=moverTorre(indice, coordDestino, turnoBlancas)
+    elif abs(difFilas)*0x10==abs(difColumnas):
+        accion=moverAlfil(indice, coordDestino, turnoBlancas)
     else:
         accion='invalido'
-
-    print(accion)
-    print(mover)
-    return
+        #print(accion)
+    return accion
 
 posBlancas=[0x12,0x22,0x32,0x42,0x52,0x62,0x72,0x82,0x11,0x81,0x21,0x71,0x31,0x61,0x41,0x51]
 posBlancasMorts=[0x01,0x02,0x03,0x04,0x94,0x93,0x92,0x91,0x10,0x80,0x20,0x70,0x30,0x60,0x40,0x50]
@@ -287,5 +368,17 @@ posNegras=[0x17,0x27,0x37,0x47,0x57,0x67,0x77,0x87,0x18,0x88,0x28,0x78,0x38,0x68
 posNegrasMorts=[0x08,0x07,0x06,0x05,0x95,0x96,0x97,0x98,0x19,0x89,0x29,0x79,0x39,0x69,0x49,0x59]
 
 turnoBlancas=True
-#moverTorre(8,0x18,turnoBlancas)
-moverAlfil(12,0x35,turnoBlancas)
+#accion=moverTorre(8,0x18,turnoBlancas)
+#accion=moverAlfil(12,0x35,turnoBlancas)
+#accion=moverReina(14,0x45,turnoBlancas)
+enpassantB=False
+columnaEnpassantB=0
+indiceEnpassantB=0
+enpassantN=False
+columnaEnpassantN=0
+indiceEnpassantN=0
+
+accion,enpassantB,columnaEnpassantB,indiceEnpassantB=moverPeon(0,0x14,turnoBlancas,enpassantB,columnaEnpassantB,indiceEnpassantB,enpassantN,columnaEnpassantN,indiceEnpassantN)
+turnoBlancas=False
+accion,enpassantN,columnaEnpassantN,indiceEnpassantN=moverPeon(1,0x13,turnoBlancas,enpassantB,columnaEnpassantB,indiceEnpassantB,enpassantN,columnaEnpassantN,indiceEnpassantN)
+print(accion)
